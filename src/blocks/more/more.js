ad48bcce5aaedd63904582531952096148f858bb
Evgeny <djek.uait1998@gmail.com>
2023-04-15 00:52:18 +0300

    some commit

--- src/blocks/more/more.js
+++ src/blocks/more/more.js
@@ -31,6 +31,13 @@ $(document).ready(function(){
                 slidesToShow: 2,
                 slidesToScroll: 2
               }
+            },
+            {
+              breakpoint: 607,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
             }
           ]
 
